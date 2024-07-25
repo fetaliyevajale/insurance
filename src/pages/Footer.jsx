66ee4e8ae5e54d4d2d077575e72f3dc726c5f24d
fetaliyevajale <fetaliyevajale@gmail.com>
2024-07-25 11:43:31 -0700

    swiper bug fixed

--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -1,8 +1,4 @@
 import React from "react";
-import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css';
-import 'swiper/css/pagination';
-import { Pagination } from 'swiper/modules';
 
 const customerReviews = [
   {
