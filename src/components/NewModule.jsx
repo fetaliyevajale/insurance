ab95349e8e432c8cfd9e6b3ff5be488059eab28f
fetaliyevajale <fetaliyevajale@gmail.com>
2024-07-31 04:35:44 -0700

    insurance Pop Up 3

--- src/components/NewModule.jsx
+++ src/components/NewModule.jsx
@@ -6,11 +6,23 @@ function NewModule({ onClose }) {
     <div className="new-module-overlay">
       <div className="new-module-content">
         <button className="new-module-close" onClick={onClose}>
-          X
+        Back to Home
         </button>
-        <h1>New Module Content</h1>
-        <p>Here you can place the content you want to show in the new module.</p>
-        {/* Əlavə məzmunu bura əlavə edə bilərsiniz */}
+        <h1 className="newModulH1">
+          <h2 className="newModulH2">
+            <img src="/tick-circle.png" alt="tick-circle" />
+          </h2>
+        </h1>
+
+        <h3 className="newModulH3">Booking Successful!</h3>
+        <h4 className="newModulH4">
+          We'd love to call with you for 30 minutes to learn more about your
+          goals and see how our product can help you achieve them.
+        </h4>
+        <button className="new-module-close" onClick={onClose}>
+        Back to Home
+        </button>
+        {}
       </div>
     </div>
   );
