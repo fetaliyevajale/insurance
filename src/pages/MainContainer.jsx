4d57d40308effc7873739d44db2f0c514d91423a
fetaliyevajale <fetaliyevajale@gmail.com>
2024-09-06 10:33:42 -0700

    mainContainerTable

--- src/pages/MainContainer.jsx
+++ src/pages/MainContainer.jsx
@@ -15,90 +15,111 @@ function MainContainer({ showDescriptions, toggleDescription }) {
           available.
         </h5>
         <div className="mainContainerDiv1Table">
-          <div
-            className={`containerRoot ${
-              showDescriptions.ssl ? "active" : ""
-            }`}
+  <div
+    className={`containerRoot ${showDescriptions.ssl ? "active" : ""}`}
+  >
+    <div className="containerRow">
+      <h1>SSL Encryption</h1>
+      <div className="img16">
+        {showDescriptions.ssl ? (
+          <img
+            src="/resim16.png"
+            alt="resim16"
             onClick={() => toggleDescription("ssl")}
-          >
-            <div className="containerRow">
-              <h1>SSL Encryption</h1>
-              <div className="img16">
-                {showDescriptions.ssl ? (
-                  <img src="/resim16.png" alt="resim16" />
-                ) : (
-                  <img src="/resim17.png" alt="resim17" />
-                )}
-              </div>
-            </div>
-            <h6 className={showDescriptions.ssl ? "visible" : "hidden"}>
-              Our website and app are both secured with 256-bit encryption.
-            </h6>
-          </div>
+          />
+        ) : (
+          <img
+            src="/resim17.png"
+            alt="resim17"
+            onClick={() => toggleDescription("ssl")}
+          />
+        )}
+      </div>
+    </div>
+    <h6 className={showDescriptions.ssl ? "visible" : "hidden"}>
+      Our website and app are both secured with 256-bit encryption.
+    </h6>
+  </div>
 
-          <div
-            className={`containerRoot ${
-              showDescriptions.account ? "active" : ""
-            }`}
+  <div
+    className={`containerRoot ${showDescriptions.account ? "active" : ""}`}
+  >
+    <div className="containerRow">
+      <h1>Account Safeguards</h1>
+      <div className="img16">
+        {showDescriptions.account ? (
+          <img
+            src="/resim16.png"
+            alt="resim16"
             onClick={() => toggleDescription("account")}
-          >
-            <div className="containerRow">
-              <h1>Account Safeguards</h1>
-              <div className="img16">
-                {showDescriptions.account ? (
-                  <img src="/resim16.png" alt="resim16" />
-                ) : (
-                  <img src="/resim17.png" alt="resim17" />
-                )}
-              </div>
-            </div>
-            <h6 className={showDescriptions.account ? "visible" : "hidden"}>
-              Our website and app are both secured with 256-bit encryption.
-            </h6>
-          </div>
+          />
+        ) : (
+          <img
+            src="/resim17.png"
+            alt="resim17"
+            onClick={() => toggleDescription("account")}
+          />
+        )}
+      </div>
+    </div>
+    <h6 className={showDescriptions.account ? "visible" : "hidden"}>
+      Our website and app are both secured with 256-bit encryption.
+    </h6>
+  </div>
 
-          <div
-            className={`containerRoot ${
-              showDescriptions.bank ? "active" : ""
-            }`}
+  <div
+    className={`containerRoot ${showDescriptions.bank ? "active" : ""}`}
+  >
+    <div className="containerRow">
+      <h1>Bank-Level Security</h1>
+      <div className="img16">
+        {showDescriptions.bank ? (
+          <img
+            src="/resim16.png"
+            alt="resim16"
             onClick={() => toggleDescription("bank")}
-          >
-            <div className="containerRow">
-              <h1>Bank-Level Security</h1>
-              <div className="img16">
-                {showDescriptions.bank ? (
-                  <img src="/resim16.png" alt="resim16" />
-                ) : (
-                  <img src="/resim17.png" alt="resim17" />
-                )}
-              </div>
-            </div>
-            <h6 className={showDescriptions.bank ? "visible" : "hidden"}>
-              Our website and app are both secured with 256-bit encryption.
-            </h6>
-          </div>
+          />
+        ) : (
+          <img
+            src="/resim17.png"
+            alt="resim17"
+            onClick={() => toggleDescription("bank")}
+          />
+        )}
+      </div>
+    </div>
+    <h6 className={showDescriptions.bank ? "visible" : "hidden"}>
+      Our website and app are both secured with 256-bit encryption.
+    </h6>
+  </div>
 
-          <div
-            className={`containerRoot ${
-              showDescriptions.privacy ? "active" : ""
-            }`}
+  <div
+    className={`containerRoot ${showDescriptions.privacy ? "active" : ""}`}
+  >
+    <div className="containerRow">
+      <h1>Privacy and Control</h1>
+      <div className="img16">
+        {showDescriptions.privacy ? (
+          <img
+            src="/resim16.png"
+            alt="resim16"
             onClick={() => toggleDescription("privacy")}
-          >
-            <div className="containerRow">
-              <h1>Privacy and Control</h1>
-              <div className="img16">
-                {showDescriptions.privacy ? (
-                  <img src="/resim16.png" alt="resim16" />
-                ) : (
-                  <img src="/resim17.png" alt="resim17" />
-                )}
-              </div>
-            </div>
-            <h6 className={showDescriptions.privacy ? "visible" : "hidden"}>
-              Our website and app are both secured with 256-bit encryption.
-            </h6>
-          </div>
-        </div>
+          />
+        ) : (
+          <img
+            src="/resim17.png"
+            alt="resim17"
+            onClick={() => toggleDescription("privacy")}
+          />
+        )}
+      </div>
+    </div>
+    <h6 className={showDescriptions.privacy ? "visible" : "hidden"}>
+      Our website and app are both secured with 256-bit encryption.
+    </h6>
+  </div>
+</div>
+
       </div>
 
       <div className="mainContainerDiv2">
